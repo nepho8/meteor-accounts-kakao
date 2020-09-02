2072868ad35eb85a3c71542b4850c3a24e2c18e7
KyungHoon <nephogram@gmail.com>
2020-09-02 10:48:41 +0900

    changed package

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: "spectrum:accounts-kakao",
-  version: '0.0.2',
+  name: 'nepho:accounts-kakao',
+  version: '0.0.3',
   summary: 'Login service for Kakao',
-  git: 'https://github.com/acidsound/meteor-accounts-kakao',
+  git: 'https://github.com/nepho8/meteor-accounts-kakao',
   documentation: 'README.md'
 });
 
@@ -11,7 +11,7 @@ Package.onUse(function(api) {
   api.use('accounts-base', ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('spectrum:kakao@0.0.3', ['client', 'server']);
+  api.use('nepho:kakao@0.0.5', ['client', 'server']);
 
   api.addFiles('kakao_login_button.css', 'client');
 
